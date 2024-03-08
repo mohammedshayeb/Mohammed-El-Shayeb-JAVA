6ec995ed0647d28264caefd6282be88119d5bc03
Mohammed El-Shayeb <145157553+mohammedshayeb@users.noreply.github.com>
2024-03-08 16:09:47 +0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+
+
+
+
+
+        
+for ( int i=1;i<=2;i++){
+    System.out.println("outer loop  #1#\t  i=  "+i);
+ for(int j =1;j<3;j++){
+    System.out.println("ensted  loop#2#\t   j= "+j+"\t i="+i);
+             
+    
+  for ( int k=1;k<=4;k++){
+System.out.println("nested loop #3#\t  k="+k+"\t j="+j+"\t i="+i);
+ }
+
+ }}
+
+
+
+for (int x=2;x<=110;x++){
+  System.out.println(x+"-\t  #  mohammed i love you ");
+
+}
+
+
+
+}
+}
